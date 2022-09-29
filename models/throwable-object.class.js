c0cb8ee886b02e04d7bda418c5f658b5be34f9ac
Jahleel <Jahleelr-h@gmx.de>
2022-09-29 02:53:09 +0200

    extracting  bug

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -6,15 +6,14 @@ class ThrowableObject extends MoveableObject {
 
     IMAGE = [
         'img/6_salsa_bottle/salsa_bottle.png'
-    ]
+    ];
 
     IMAGES_ROTATION = [
         'img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
         'img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
         'img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
         'img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png'
-    ]
-
+    ];
     IMAGES_SALSA_SPLASH = [
         'img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png',
         'img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png',
@@ -22,17 +21,17 @@ class ThrowableObject extends MoveableObject {
         'img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png',
         'img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png',
         'img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png'
-    ]
+    ];
 
 
     constructor(x, y) {
         super().loadImage(this.IMAGE);
-        //this.loadImage(this.IMAGES_ROTATION);
-        //this.loadImage(this.IMAGES_SALSA_SPLASH);
+
         this.x = x;
         this.y = y;
+        //this.loadImage(this.IMAGES_ROTATION);
+        //this.loadImage(this.IMAGES_SALSA_SPLASH);
         this.trow();
-
         this.bottleAnimation();
     }
 
@@ -48,7 +47,7 @@ class ThrowableObject extends MoveableObject {
     bottleAnimation() {
         setInterval(() => {
             if (this.bottleHitsEnemy = true) {
-                this.stopBottleAndSplash();
+                //this.stopBottleAndSplash();
             } else {
                 this.spinBottle();
             }
